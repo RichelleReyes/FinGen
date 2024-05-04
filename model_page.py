d9f01c8ceef79b5dfe713a5b330dfbb38016b027
secretarybird97 <81328194+secretarybird97@users.noreply.github.com>
2024-05-04 03:34:25 -0700

    Chat window render in selected scenario

--- model_page.py
+++ model_page.py
@@ -1,13 +1,19 @@
 import streamlit as st
-import stTools as tools
-from models.MonteCarloSimulator import Monte_Carlo_Simulator
+
 import model_page_components
+from chat import load_chat_window
+
 
 def load_page() -> None:
-    st.markdown("<h2 style='color: #000; font-size: 3rem;'>Hello, Team</h2>", unsafe_allow_html=True)
-    
+    st.markdown(
+        "<h2 style='color: #000; font-size: 3rem;'>Hello, Team</h2>",
+        unsafe_allow_html=True,
+    )
+
     # Call other components or functions in your module
-    model_page_components.add_portfolio_risk_score(model_page_components.calculate_score())
+    model_page_components.add_portfolio_risk_score(
+        model_page_components.calculate_score()
+    )
     if st.session_state["generate_analysis"]:  # Check if button is clicked
         model_page_components.add_portfolio_graph()
         model_page_components.add_risk_profile_chart()
@@ -17,6 +23,4 @@ def load_page() -> None:
         # st.session_state["generate_analysis"] = False
         asset_data = model_page_components.generate_asset_data(selected_scenario)
         st.plotly_chart(model_page_components.visualize_asset_data(asset_data))
-
-
-
+        load_chat_window()
