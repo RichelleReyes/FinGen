666b6b60fffb9681ea0c98c633d651084b5d3dbf
SebastianA21 <sebastian.arriaga@uabc.edu.mx>
2024-05-04 02:35:35 -0700

    Simulation Generation

--- model_page.py
+++ model_page.py
@@ -8,8 +8,15 @@ def load_page() -> None:
     
     # Call other components or functions in your module
     model_page_components.add_portfolio_risk_score(model_page_components.calculate_score())
-    model_page_components.add_portfolio_graph()
+    if st.session_state["generate_analysis"]:  # Check if button is clicked
+        model_page_components.add_portfolio_graph()
+        model_page_components.add_risk_profile_chart()
+    selected_scenario = model_page_components.add_simulation_scenarios()
 
+    if selected_scenario is not None:
+        # st.session_state["generate_analysis"] = False
+        asset_data = model_page_components.generate_asset_data(selected_scenario)
+        st.plotly_chart(model_page_components.visualize_asset_data(asset_data))
 
 
 
